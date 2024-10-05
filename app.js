4f2cc92e02acdfe6ddfd3f2ad874005ff1e1e643
Shady Waheed <shekohitler@gmail.com>
2024-10-05 15:59:43 +0300

    Implemented CORS

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const xss = require('xss-clean');
 const hpp = require('hpp');
 const cookieParser = require('cookie-parser');
 const compression = require('compression');
+const cors = require('cors');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -28,6 +29,16 @@ if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 // 1) Global Middlewares
+// Implement CORS
+app.use(cors())
+// Access-Control-Allow-Origin *
+// api.natours.com, front-end natours.com
+// app.use(cors({
+//   origin: 'https://www.natours.com'
+// })) 
+
+app.options('*', cors())
+// app.options('/api/v1/tours/:id', cors());
 
 // Serving static files
 app.use(express.static(path.join(__dirname, 'public')));
